755654bbf9e7ba312f4ed66561631634b7b10fc0
JavierValenciaR <javier_valencia_r@hotmail.com>
2024-11-07 20:24:42 -0500

    cambios

---
+++ 03_TIPO_DE_DATOS/00_RESOURCES/JValencia/mapas.dart
@@ -0,0 +1,17 @@
+void main() {
+  Map<String, dynamic> producto = {
+                                    "id":1,
+                                    "nombre":"celular",
+                                    "codigo_barras":"11234567890",
+                                    "precio":1099.99,
+                                    "descripcion":"celular de alta gama",
+                                    "imagen":"https://test.com/img.img",
+                                    "EstaDisponible":true,
+                                    "images":["uno","dos","tres"],
+                                    "colors": {
+                                            "iphone6":"rojo",
+                                            "iphone11":"azul"
+                                    }
+  };
+  
+}
